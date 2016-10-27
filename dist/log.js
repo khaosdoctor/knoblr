42723df733788325d6703a72748d23b4bc82c073
Lucas Santos <lhs.santoss@gmail.com>
2016-10-27 17:11:05 -0200

    Added cool new stuff for 2.0

--- dist/log.js
+++ dist/log.js
@@ -2,16 +2,50 @@
 
 var moment = require('moment');
 var chalk = require('chalk');
-var now = chalk.italic('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] =>');
+
+var datetime_format = 'YYYY-MM-DD HH:mm:ss';
+var log_types = {
+  "warn": chalk.yellow,
+  "error": chalk.red,
+  "info": chalk.cyan
+};
+var display_logtype = true;
+var display_timestamp = true;
+var now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
 
 module.exports = {
+  displayTimestamp: function displayTimestamp(v) {
+    display_timestamp = v;
+  },
+  displayLogType: function displayLogType(v) {
+    display_logtype = v;
+  },
+  "colors": chalk,
+  reset: function reset() {
+    display_logtype = true;
+    display_timestamp = true;
+    datetime_format = 'YYYY-MM-DD HH:mm:ss';
+    now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
+    log_types = {
+      "warn": chalk.yellow,
+      "error": chalk.red,
+      "info": chalk.cyan
+    };
+  },
+  setLogColor: function setLogColor(type, color) {
+    log_types[type] = color;
+  },
+  setTimeFormat: function setTimeFormat(f) {
+    datetime_format = f;
+    now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
+  },
   info: function info(t) {
-    return console.info(now, chalk.cyan.bold('{INFO}: ') + chalk.cyan(t));
+    return console.info(display_timestamp ? now : "", display_logtype ? log_types.info.bold('{INFO}:') : "", log_types.info(t));
   },
   warn: function warn(t) {
-    return console.warn(now, chalk.yellow.bold('{WARN}: ') + chalk.yellow(t));
+    return console.warn(display_timestamp ? now : "", display_logtype ? log_types.warn.bold('{WARN}:') : "", log_types.warn(t));
   },
   error: function error(t) {
-    return console.error(now, chalk.red.bold('{ERROR}: ') + chalk.red(t));
+    return console.error(display_timestamp ? now : "", display_logtype ? log_types.error.bold('{ERROR}:') : "", log_types.error(t));
   }
 };
--- src/log.js
+++ src/log.js
@@ -1,9 +1,57 @@
 const moment = require('moment');
 const chalk = require('chalk');
-let now = chalk.italic('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] =>');
+
+let datetime_format = 'YYYY-MM-DD HH:mm:ss';
+let log_types = {
+  "warn": chalk.yellow,
+  "error": chalk.red,
+  "info": chalk.cyan
+};
+let display_logtype = true;
+let display_timestamp = true;
+let now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
 
 module.exports = {
-  info: (t) => { return console.info(now, chalk.cyan.bold('{INFO}: ') + chalk.cyan(t)); },
-  warn: (t) => { return console.warn(now, chalk.yellow.bold('{WARN}: ') + chalk.yellow(t)); },
-  error: (t) => { return console.error(now, chalk.red.bold('{ERROR}: ') + chalk.red(t)); },
+  displayTimestamp: (v) => { display_timestamp = v; },
+  displayLogType: (v) => { display_logtype = v; },
+  "colors": chalk,
+  reset: () => {
+    display_logtype = true;
+    display_timestamp = true;
+    datetime_format = 'YYYY-MM-DD HH:mm:ss';
+    now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
+    log_types = {
+      "warn": chalk.yellow,
+      "error": chalk.red,
+      "info": chalk.cyan
+    };
+  },
+  setLogColor: (type, color) => {
+    log_types[type] = color;
+  },
+  setTimeFormat: (f) => {
+    datetime_format = f;
+    now = chalk.italic('[' + moment().format(datetime_format) + '] =>');
+  },
+  info: (t) => { 
+    return console.info(
+      (display_timestamp) ? now : "",
+      (display_logtype) ? log_types.info.bold('{INFO}:') : "",
+      log_types.info(t)
+    ); 
+  },
+  warn: (t) => { 
+    return console.warn(
+      (display_timestamp) ? now : "",
+      (display_logtype) ? log_types.warn.bold('{WARN}:') : "",
+      log_types.warn(t)
+    ); 
+  },
+  error: (t) => { 
+    return console.error(
+      (display_timestamp) ? now : "",
+      (display_logtype) ? log_types.error.bold('{ERROR}:') : "",
+      log_types.error(t)
+    ); 
+  },
 }
