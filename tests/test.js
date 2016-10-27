eed422e4c1950d273fc213f4554657fae4b466fa
Lucas Santos <lhs.santoss@gmail.com>
2016-10-27 20:29:14 -0200

    Added tests and testrunners

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+
+test('Changing colors', t => {
+  t.pass("Testing");
+});
+
+test('Changing date format', t => {
+  t.pass("Testing");
+});
+
+test('Removing log type', t => {
+  t.pass("Testing");
+});
+
+test('Removing timestamp', t => {
+  t.pass("Testing");
+});
